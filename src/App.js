a435b3b8ff12deead6b06616d0e4986dd9354ef3
Shirley Wu <shirleyxueyangwu@gmail.com>
2016-10-27 18:16:57 -0700

    import annotations_subtitles.json in App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,8 @@ import _ from 'lodash';
 
 import videosData from './data/videos.json';
 import subtitlesData from './data/all_subtitles.json';
-
-var annotationsData = _.mapValues(subtitlesData, (subtitle, videoKey) => {
-  return require('./annotations/' + videoKey + '.json');
-});
+import annotationsData from './data/annotation_subtitles.json';
+import showsData from './data/shows.json';
 
 var emojis = {
   happy: '😆',
@@ -33,29 +31,6 @@ var emojis = {
 
 var App = React.createClass({
   componentWillMount() {
-    var annotationsSubtitles = [];
-    _.each(annotationsData, (annotations, videoId) => {
-      var subtitleByStart = _.groupBy(subtitlesData[videoId], 'start');
-      _.each(annotations, (annotation, filename) => {
-        var time = filename.match(/at-([\d\.]+).png/);
-        time = parseFloat(time[1]) * 1000;
-        var subtitle = subtitleByStart[time];
-        if (!subtitle) return;
-
-        annotationsSubtitles.push(Object.assign({
-          start: subtitle[0].start,
-          end: subtitle[0].end,
-          words: _.map(subtitle, 'words').join(' '),
-          videoId,
-          filename,
-        }, annotation));
-      });
-    });
-
-    // console.log(annotationsSubtitles);
-    console.log(_.chain(annotationsSubtitles).map('faces').flatten().countBy().toPairs().sortBy(1).value());
-    // console.log(_.chain(annotationsSubtitles).map('labels').flatten().countBy().toPairs().sortBy(1).value());
-    this.setState({data: annotationsSubtitles});
   },
 
   randomize() {
@@ -66,7 +41,7 @@ var App = React.createClass({
     var imageHeight = 360;
     var emotions = ['happy', 'happy', 'happy', 'happy'];
     var faces = _.map(emotions, (emotion) => {
-      var emotionObj = _.filter(this.state.data, d => d.faces.length && d.faces[0][emotion]);
+      var emotionObj = _.filter(annotationsData, d => d.faces.length && d.faces[0][emotion]);
       if (!emotionObj.length) return;
       emotionObj = emotionObj[_.random(emotionObj.length - 1)];
 
