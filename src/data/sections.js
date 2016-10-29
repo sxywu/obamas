423afe5dde3a6e5ebdcf121664a3765d1f1e1d80
Shirley Wu <shirleyxueyangwu@gmail.com>
2016-10-29 16:28:50 -0700

    Save local changes

--- src/data/sections.js
+++ src/data/sections.js
@@ -1,7 +1,17 @@
+import * as d3 from 'd3';
+
+var simulation = d3.forceSimulation()
+  .force('charge', d3.forceCollide(d => d.radius))
+  .force('x', d3.forceX(d => d.focusX))
+  .force('y', d3.forceY(d => d.focusY));
 
 export default function(data) {
   return [
     {
+      id: 'all_hosts',
+      position() {
+        // position
+      },
       text: `
 
       `
