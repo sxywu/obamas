45f11cbac7abae1107acd4ea288b89d2ddae581b
Shirley Wu <shirleyxueyangwu@gmail.com>
2016-10-29 22:26:50 -0700

    position obamas

--- src/data/sections.js
+++ src/data/sections.js
@@ -2,18 +2,16 @@ import * as d3 from 'd3';
 import _ from 'lodash';
 
 var padding = {top: 20, left: 20};
-var simulation = d3.forceSimulation()
-  .force('charge', d3.forceCollide(d => d.radius))
-  .force('x', d3.forceX(d => d.focusX))
-  .force('y', d3.forceY(d => d.focusY))
-  .stop();
 
 export default function(data, images) {
   return [
     {
       id: 'all_hosts',
       position(width, top) {
-        width *= 0.8;
+        width *= 0.75;
+
+        var hostSize = 50;
+        var obamaSize = 40;
 
         // position hosts first
         var perRow = 5;
@@ -22,37 +20,51 @@ export default function(data, images) {
           .sortBy(show => -show.dates.length)
           .map((show, i) => {
             var x = (i % perRow + 0.5) * perWidth + padding.left;
-            var y = 2 * (Math.floor(i / perRow) + 0.5) * perWidth + top;
+            var y = 1.75 * (Math.floor(i / perRow) + 0.5) * perWidth + top;
 
             return {
               x,
               y,
-              radius: 30,
+              radius: hostSize,
               host: show.host,
               image: show.image,
             };
           }).value();
         var hostsByKey = _.keyBy(hosts, 'host');
 
+        var perRow = 3;
         var obamas = _.chain(data.showsData)
           .map(show => {
             var host = hostsByKey[show.host];
-            return _.map(show.dates, data => {
-              var [date, guest] = data;
-              return {
-                focusX: host.x,
-                focusY: host.y + perWidth,
-                image: images[guest],
-                guest,
-                radius: 10,
-              };
-            });
-          }).flatten().value()
+            var length = show.dates.length;
+            var rows = Math.floor(length / perRow);
+            var extras = length - rows * perRow;
 
-        simulation.nodes(obamas);
-        _.times(1000, i => {
-          simulation.tick();
-        });
+            return _.chain(show.dates)
+              .sortBy(date => date[1])
+              .map((data, i) => {
+                var [date, guest] = data;
+                var y = Math.floor(i / perRow);
+                var x = (i % perRow + 0.5) * obamaSize;
+
+                if (y === rows) {
+                  // if it's the last row
+                  x = x - (extras * obamaSize) / 2;
+                } else {
+                  x = x - (perRow * obamaSize) / 2;
+                }
+                y = y * obamaSize;
+
+                return {
+                  x: host.x + x,
+                  y: host.y + 1.5 * host.radius + y,
+                  image: images[guest],
+                  date,
+                  guest,
+                  radius: obamaSize * 0.85,
+                };
+              }).value();
+          }).flatten().value()
 
         return {hosts, obamas};
       },
