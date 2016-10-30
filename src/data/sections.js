063bbb86f6df2f30063fdf773747d725737262f6
Shirley Wu <shirleyxueyangwu@gmail.com>
2016-10-29 21:46:39 -0700

    sort hosts by number of appearances

--- src/data/sections.js
+++ src/data/sections.js
@@ -13,23 +13,25 @@ export default function(data, images) {
     {
       id: 'all_hosts',
       position(width, top) {
-        width *= 0.75;
+        width *= 0.8;
 
         // position hosts first
         var perRow = 5;
-        var perWidth = (width - (perRow - 1) * padding.left - 2 * padding.left) / (perRow + 1);
-        var hosts = _.map(data.showsData, (show, i) => {
-          var x = (i % perRow + 0.5) * perWidth + padding.left;
-          var y = 2 * (Math.floor(i / perRow) + 0.5) * perWidth + top;
+        var perWidth = width / (perRow + 1);
+        var hosts = _.chain(data.showsData)
+          .sortBy(show => -show.dates.length)
+          .map((show, i) => {
+            var x = (i % perRow + 0.5) * perWidth + padding.left;
+            var y = 2 * (Math.floor(i / perRow) + 0.5) * perWidth + top;
 
-          return {
-            x,
-            y,
-            radius: 30,
-            host: show.host,
-            image: show.image,
-          };
-        });
+            return {
+              x,
+              y,
+              radius: 30,
+              host: show.host,
+              image: show.image,
+            };
+          }).value();
         var hostsByKey = _.keyBy(hosts, 'host');
 
         var obamas = _.chain(data.showsData)
